09ab9653129e642c076414667160ae4387fae0ac
Violet <1054197367@qq.com>
2022-12-22 20:37:05 +0800

    添加开发服务器配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 开发服务器配置 
+  // https://cn.vitejs.dev/config/server-options.html#server-proxy
+  server: {
+    port: 3333,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "",
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
