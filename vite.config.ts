a7f5c913d1c1b41fa5cc3744a999ddfe2998acff
Violet <1054197367@qq.com>
2022-12-22 22:14:34 +0800

    bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '.',
+    base: './',
     plugins: [
         vue(),
         // 按需加载ElementPlus组件
